8424554707a70531cc354efd4fc6f174370d3d22
Eigensystem <eigensystem1318@gmail.com>
2022-05-10 13:50:23 +0800

    accelerated with NEON simd

--- Hust-Heptagon-recruitment-2022-spring/task3-gemm-thread/main.cpp
+++ Hust-Heptagon-recruitment-2022-spring/task3-gemm-thread/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cassert>
 #include <thread>
+#include <arm_neon.h>
 
 #define PRINT_TIME(code) do { \
     auto start = system_clock::now(); \
@@ -25,7 +26,6 @@ using vec = vector<int>;
 const int scale[] = {256, 512, 1024, 2048};
 const string data_path("./data/");
 const int num_threads = 8; 
-
 
 
 void transpose(const int size, const vec input, vec &result, 
@@ -47,11 +47,24 @@ void multi_threads_gemm(const int size, const vec a, const vec b,
     int end = single_processing_line * (thread_id + 1);
     for(int i = start; i < end; ++i){
         for(int j = 0; j < size; ++j){
-            int sum = 0;
-            int k;
-            for(k = 0; k < size; ++k){
-                sum += a[i * size + k] * b[j * size + k];
+            int sum;
+            int32x4_t sum_vec;
+            int32x4x4_t sum_group;
+            int32x4x4_t a_vec, b_vec;
+            sum_group.val[0] = {0, 0, 0, 0};
+            sum_group.val[1] = {0, 0, 0, 0};
+            sum_group.val[2] = {0, 0, 0, 0};
+            sum_group.val[3] = {0, 0, 0, 0};
+            for(int k = 0; k < size; k += 16){
+                a_vec = vld1q_s32_x4((int *)&a[i * size + k]);
+                b_vec = vld1q_s32_x4((int *)&b[j * size + k]);
+                sum_group.val[0] = vmlaq_s32(sum_group.val[0], a_vec.val[0], b_vec.val[0]);
+                sum_group.val[1] = vmlaq_s32(sum_group.val[1], a_vec.val[1], b_vec.val[1]);
+                sum_group.val[2] = vmlaq_s32(sum_group.val[2], a_vec.val[2], b_vec.val[2]);
+                sum_group.val[3] = vmlaq_s32(sum_group.val[3], a_vec.val[3], b_vec.val[3]);
             }
+            sum_vec = vaddq_s32(vaddq_s32(sum_group.val[0], sum_group.val[1]), vaddq_s32(sum_group.val[2], sum_group.val[3]));
+            sum = sum_vec[0] + sum_vec[1] + sum_vec[2] + sum_vec[3];
             c[i * size + j] = sum;
         }
     }
