5b305e5ca858b914489458216e7967866d1ddfbf
Eigensystem <eigensystem1318@gmail.com>
2022-05-12 05:14:09 +0800

    Accelerate_4.0_hust_heptagon-recruitment

--- Hust-Heptagon-recruitment-2022-spring/task3-gemm/main.cpp
+++ Hust-Heptagon-recruitment-2022-spring/task3-gemm/main.cpp
@@ -26,7 +26,8 @@ using vec = vector<int>;
 
 const int scale[] = {256, 512, 1024, 2048};
 const string data_path("./data/");
-const int num_threads = 8; 
+const int num_threads = 8;
+int block = 256;
 
 int a[4194304] __attribute__((__aligned__((0x100))));
 int b[4194304] __attribute__((__aligned__((0x100))));
@@ -49,55 +50,105 @@ void multi_threads_gemm(const int size, int thread_id){
     int start = single_processing_line * thread_id;
     int end = single_processing_line * (thread_id + 1);
     int i = start;
-    int byte_a_line = 4 * size;
-    int sum;
-    int32x4_t sum_vec;
-    int32x4_t sum_group_val_0, sum_group_val_1, sum_group_val_2, sum_group_val_3;
+    int sum_00, sum_01, sum_02, sum_03;
+    int sum_10, sum_11, sum_12, sum_13;
+    int sum_20, sum_21, sum_22, sum_23;
+    int sum_30, sum_31, sum_32, sum_33;
+    int32x4_t sum_group_val_00, sum_group_val_10, sum_group_val_20, sum_group_val_30;
+    int32x4_t sum_group_val_01, sum_group_val_11, sum_group_val_21, sum_group_val_31;
+    int32x4_t sum_group_val_02, sum_group_val_12, sum_group_val_22, sum_group_val_32;
+    int32x4_t sum_group_val_03, sum_group_val_13, sum_group_val_23, sum_group_val_33;
     int32x4_t a_vec_val_0, a_vec_val_1, a_vec_val_2, a_vec_val_3;
     int32x4_t b_vec_val_0, b_vec_val_1, b_vec_val_2, b_vec_val_3;
-    int32x4_t * a_addr = (int32x4_t *)((char *)&a + start * byte_a_line);
-    int32x4_t * a_group_addr_now;
-    int32x4_t * b_group_addr_now;
-    do {
-        int j = 0;
-        int32x4_t * b_transport_addr = (int32x4_t *)&b_transport;
-        do {
-            int k = 0;
-            sum_group_val_0 = {0, 0, 0, 0};
-            sum_group_val_1 = {0, 0, 0, 0};
-            sum_group_val_2 = {0, 0, 0, 0};
-            sum_group_val_3 = {0, 0, 0, 0};
-            a_group_addr_now = a_addr;
-            b_group_addr_now = b_transport_addr;
-            do {
-                a_vec_val_0 = a_group_addr_now[0];
-                a_vec_val_1 = a_group_addr_now[1];
-                a_vec_val_2 = a_group_addr_now[2];
-                a_vec_val_3 = a_group_addr_now[3];
-                a_group_addr_now += 4;
-                b_vec_val_0 = b_group_addr_now[0];
-                b_vec_val_1 = b_group_addr_now[1];
-                b_vec_val_2 = b_group_addr_now[2];
-                b_vec_val_3 = b_group_addr_now[3];
-                b_group_addr_now += 4;
-                sum_group_val_0 = vmlaq_s32(sum_group_val_0, b_vec_val_0, a_vec_val_0);
-                sum_group_val_1 = vmlaq_s32(sum_group_val_1, b_vec_val_1, a_vec_val_1);
-                sum_group_val_2 = vmlaq_s32(sum_group_val_2, b_vec_val_2, a_vec_val_2);
-                sum_group_val_3 = vmlaq_s32(sum_group_val_3, b_vec_val_3, a_vec_val_3);
-                k += 16;
-            } while(k < size);
-            sum_vec = vaddq_s32(vaddq_s32(vaddq_s32(sum_group_val_0, sum_group_val_1), sum_group_val_2), sum_group_val_3);
-            sum = sum_vec[0] + sum_vec[1] + sum_vec[2] + sum_vec[3];
-            c[i * size + j] = sum;
-            b_transport_addr = (int32x4_t *)((char *)b_transport_addr + byte_a_line);
-            ++j;
-        } while(j != size);
-        ++i;
-        a_addr = (int32x4_t *)((char *)a_addr + byte_a_line);
-    } while(i != end);
+    int32x4_t * a_addr_init = (int32x4_t *)((int *)&a + start * size);
+    int32x4_t * a_group_addr_now_0, * a_group_addr_now_1, * a_group_addr_now_2, * a_group_addr_now_3;
+    int32x4_t * b_group_addr_now_0, * b_group_addr_now_1, * b_group_addr_now_2, * b_group_addr_now_3;
+    int32x4_t * b_transport_addr;
+    if(block > size)
+        block = size;
+    for(int m = 0; m < size; m += block) {
+        int row_block_limit = m + block;
+        for(int n = 0; n < size; n += block) {
+            int32x4_t * a_addr = (int32x4_t *)((int *)a_addr_init + n);
+            for(i = start; i < end; i += 4) {
+                b_transport_addr = (int32x4_t *)((int *)&b_transport + m * size + n);
+                for(int j = m; j < row_block_limit; j += 4) {
+                    sum_group_val_00 = {0, 0, 0, 0}; sum_group_val_01 = {0, 0, 0, 0}; sum_group_val_02 = {0, 0, 0, 0}; sum_group_val_03 = {0, 0, 0, 0};
+                    sum_group_val_10 = {0, 0, 0, 0}; sum_group_val_11 = {0, 0, 0, 0}; sum_group_val_12 = {0, 0, 0, 0}; sum_group_val_13 = {0, 0, 0, 0};
+                    sum_group_val_20 = {0, 0, 0, 0}; sum_group_val_21 = {0, 0, 0, 0}; sum_group_val_22 = {0, 0, 0, 0}; sum_group_val_23 = {0, 0, 0, 0};
+                    sum_group_val_30 = {0, 0, 0, 0}; sum_group_val_31 = {0, 0, 0, 0}; sum_group_val_32 = {0, 0, 0, 0}; sum_group_val_33 = {0, 0, 0, 0}; 
+                    a_group_addr_now_0 = a_addr;
+                    a_group_addr_now_1 = (int32x4_t *)((int *)a_addr + size);
+                    a_group_addr_now_2 = (int32x4_t *)((int *)a_addr + 2 * size);
+                    a_group_addr_now_3 = (int32x4_t *)((int *)a_addr + 3 * size);
+                    b_group_addr_now_0 = b_transport_addr;
+                    b_group_addr_now_1 = (int32x4_t *)((int *)b_transport_addr + size);
+                    b_group_addr_now_2 = (int32x4_t *)((int *)b_transport_addr + 2 * size);
+                    b_group_addr_now_3 = (int32x4_t *)((int *)b_transport_addr + 3 * size);
+                    for(int k = 0; k < block; k += 4) {
+                        a_vec_val_0 = *a_group_addr_now_0;
+                        a_vec_val_1 = *a_group_addr_now_1;
+                        a_vec_val_2 = *a_group_addr_now_2;
+                        a_vec_val_3 = *a_group_addr_now_3;
+                        ++a_group_addr_now_0;
+                        ++a_group_addr_now_1;
+                        ++a_group_addr_now_2;
+                        ++a_group_addr_now_3;
+                        b_vec_val_0 = *b_group_addr_now_0;
+                        b_vec_val_1 = *b_group_addr_now_1;
+                        b_vec_val_2 = *b_group_addr_now_2;
+                        b_vec_val_3 = *b_group_addr_now_3;
+                        ++b_group_addr_now_0;
+                        ++b_group_addr_now_1;
+                        ++b_group_addr_now_2;
+                        ++b_group_addr_now_3;
+                        sum_group_val_00 = vmlaq_s32(sum_group_val_00, b_vec_val_0, a_vec_val_0);
+                        sum_group_val_01 = vmlaq_s32(sum_group_val_01, b_vec_val_1, a_vec_val_0);
+                        sum_group_val_02 = vmlaq_s32(sum_group_val_02, b_vec_val_2, a_vec_val_0);
+                        sum_group_val_03 = vmlaq_s32(sum_group_val_03, b_vec_val_3, a_vec_val_0);
+                        sum_group_val_10 = vmlaq_s32(sum_group_val_10, b_vec_val_0, a_vec_val_1);
+                        sum_group_val_11 = vmlaq_s32(sum_group_val_11, b_vec_val_1, a_vec_val_1);
+                        sum_group_val_12 = vmlaq_s32(sum_group_val_12, b_vec_val_2, a_vec_val_1);
+                        sum_group_val_13 = vmlaq_s32(sum_group_val_13, b_vec_val_3, a_vec_val_1);
+                        sum_group_val_20 = vmlaq_s32(sum_group_val_20, b_vec_val_0, a_vec_val_2);
+                        sum_group_val_21 = vmlaq_s32(sum_group_val_21, b_vec_val_1, a_vec_val_2);
+                        sum_group_val_22 = vmlaq_s32(sum_group_val_22, b_vec_val_2, a_vec_val_2);
+                        sum_group_val_23 = vmlaq_s32(sum_group_val_23, b_vec_val_3, a_vec_val_2);
+                        sum_group_val_30 = vmlaq_s32(sum_group_val_30, b_vec_val_0, a_vec_val_3);
+                        sum_group_val_31 = vmlaq_s32(sum_group_val_31, b_vec_val_1, a_vec_val_3);
+                        sum_group_val_32 = vmlaq_s32(sum_group_val_32, b_vec_val_2, a_vec_val_3);
+                        sum_group_val_33 = vmlaq_s32(sum_group_val_33, b_vec_val_3, a_vec_val_3);
+                    }
+                    sum_00 = sum_group_val_00[0] + sum_group_val_00[1] + sum_group_val_00[2] + sum_group_val_00[3];
+                    sum_01 = sum_group_val_01[0] + sum_group_val_01[1] + sum_group_val_01[2] + sum_group_val_01[3];
+                    sum_02 = sum_group_val_02[0] + sum_group_val_02[1] + sum_group_val_02[2] + sum_group_val_02[3];
+                    sum_03 = sum_group_val_03[0] + sum_group_val_03[1] + sum_group_val_03[2] + sum_group_val_03[3];
+                    sum_10 = sum_group_val_10[0] + sum_group_val_10[1] + sum_group_val_10[2] + sum_group_val_10[3];
+                    sum_11 = sum_group_val_11[0] + sum_group_val_11[1] + sum_group_val_11[2] + sum_group_val_11[3];
+                    sum_12 = sum_group_val_12[0] + sum_group_val_12[1] + sum_group_val_12[2] + sum_group_val_12[3];
+                    sum_13 = sum_group_val_13[0] + sum_group_val_13[1] + sum_group_val_13[2] + sum_group_val_13[3];
+                    sum_20 = sum_group_val_20[0] + sum_group_val_20[1] + sum_group_val_20[2] + sum_group_val_20[3];
+                    sum_21 = sum_group_val_21[0] + sum_group_val_21[1] + sum_group_val_21[2] + sum_group_val_21[3];
+                    sum_22 = sum_group_val_22[0] + sum_group_val_22[1] + sum_group_val_22[2] + sum_group_val_22[3];
+                    sum_23 = sum_group_val_23[0] + sum_group_val_23[1] + sum_group_val_23[2] + sum_group_val_23[3];
+                    sum_30 = sum_group_val_30[0] + sum_group_val_30[1] + sum_group_val_30[2] + sum_group_val_30[3];
+                    sum_31 = sum_group_val_31[0] + sum_group_val_31[1] + sum_group_val_31[2] + sum_group_val_31[3];
+                    sum_32 = sum_group_val_32[0] + sum_group_val_32[1] + sum_group_val_32[2] + sum_group_val_32[3];
+                    sum_33 = sum_group_val_33[0] + sum_group_val_33[1] + sum_group_val_33[2] + sum_group_val_33[3];
+                    c[  i   * size + j + 0] += sum_00; c[  i   * size + j + 1] += sum_01; c[  i   * size + j + 2] += sum_02; c[  i   * size + j + 3] += sum_03;
+                    c[(i+1) * size + j + 0] += sum_10; c[(i+1) * size + j + 1] += sum_11; c[(i+1) * size + j + 2] += sum_12; c[(i+1) * size + j + 3] += sum_13;
+                    c[(i+2) * size + j + 0] += sum_20; c[(i+2) * size + j + 1] += sum_21; c[(i+2) * size + j + 2] += sum_22; c[(i+2) * size + j + 3] += sum_23;
+                    c[(i+3) * size + j + 0] += sum_30; c[(i+3) * size + j + 1] += sum_31; c[(i+3) * size + j + 2] += sum_32; c[(i+3) * size + j + 3] += sum_33;
+                    b_transport_addr = (int32x4_t *)((int *)b_transport_addr + 4 * size);
+                }
+                a_addr = (int32x4_t *)((int *)a_addr + 4 * size);
+            }
+        }
+    }
 }
 
 void Gemm(const int &size) {
+    memset(&c, 0, 4194304);
     std::thread workers[num_threads];
     for(int i = 1; i < num_threads; ++i){
         workers[i] = std::thread(transpose, size, i);
